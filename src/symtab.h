4c06fca9045613bead55acbb2c543d35e1d60e54
Alexei Frolov <alexeifrolov3@gmail.com>
2017-02-06 12:34:08 -0500

    Change way type is stored

--- src/symtab.h
+++ src/symtab.h
@@ -5,23 +5,28 @@
 #ifndef SYMTAB_H
 #define SYMTAB_H
 
-#define TYPE_INT        (1 << 0)
-#define TYPE_CHAR       (1 << 1)
-#define TYPE_VOID       (1 << 2)
+enum {
+	TYPE_INT = 1,
+	TYPE_CHAR,
+	TYPE_VOID,
+	TYPE_STRLIT
+};
 
+#define PROPERTY_FUNC   (1 << 8)
 #define QUAL_UNSIGNED   (1 << 16)
 
 #include "uthash.h"
 
+#define FLAGS_TYPE(x) ((x) & 0xF)
+
 /*
  * An entry in the symbol table.
  * The flags bitfield is set up as follows:
- * PPPPPPPPxxxxxxxUxxxxxVCI
+ * PPPPPPPPxxxxxxxUxxxxxxxFxxxxTTTT
  * P: level of indirection
  * U: unsigned flag
- * V: void type
- * C: char type
- * I: int type
+ * F: is a function
+ * T: type
  */
 struct symbol {
 	char            *id;
