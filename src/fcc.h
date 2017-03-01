66e5b7cd14029e9ee246a3c59f2418d64f4d1c73
Alexei Frolov <alexeifrolov3@gmail.com>
2017-03-01 14:42:07 -0500

    Alignment macros

--- src/fcc.h
+++ src/fcc.h
@@ -8,4 +8,8 @@
 extern char *fcc_filename;
 extern void *fcc_scanner;
 
+#define ALIGN(x, a)             __ALIGN_MASK(x, (a) - 1)
+#define __ALIGN_MASK(x, mask)   (((x) + (mask)) & ~(mask))
+#define ALIGNED(x, a)           (((x) & ((a) - 1)) == 0)
+
 #endif /* FCC_FCC_H */
